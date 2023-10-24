5b6a395089c286f136074feac701d6f4b0c267e4
Attstar2021 <>
2023-10-24 16:44:45 +0000

    removed unused code which was commented out

--- run.py
+++ run.py
@@ -40,9 +40,9 @@ def validate_data(values):
     """
     Inside the try, converts all string values into intergers.
     raises valueError if strings cannot be converted into int,
-    or ti there aren't exaclty 6 values.
+    or if there aren't exaclty 6 values.
     """
-    try: 
+    try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
@@ -53,28 +53,6 @@ def validate_data(values):
         return False
 
     return True
-
-""" just to remember how i did it first time
-
-def update_sales_worksheet(data):
-    "
-    update sales worksheetm add new rowwith the list data provided.
-    "
-    print("Updating sales worksheet... \n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n ")
-
-
-def update_surplus_worksheet(data):
-    "
-    Update surplus worksheet add new rowwith the list data provided.
-    "
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.")
-    """
 
 
 def update_worksheet(data, worksheet):
